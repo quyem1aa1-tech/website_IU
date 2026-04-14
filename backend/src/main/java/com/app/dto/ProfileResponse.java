8119b1886fbeb8c1170972e3be0c7f4a8587887c
TruongTheVinh <quyem1aa1@gmail.com>
2026-04-14 23:43:27 +0700

    fix

--- backend/src/main/java/com/app/dto/ProfileResponse.java
+++ backend/src/main/java/com/app/dto/ProfileResponse.java
@@ -1,34 +1,13 @@
 package com.app.dto;
+import lombok.*;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfileResponse {
     private String username;
     private String fullName;
     private String studentId;
     private String email;
 
-    // Constructor
-    public ProfileResponse(String studentId, String username, String fullName, String email) {
-        this.studentId = studentId;
-        this.username = username;
-        this.fullName = fullName;
-        this.email = email;
-    }
-
-    // Getters and Setters (Important for JSON conversion)
-    public String getUsername() {
-        return username;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
